9d7bef861c822c305d8165066e16420916139481
Jane Stanton <stanjane@gmail.com>
2013-06-10 13:59:08 -0500

    commit

---
+++ animate.js
@@ -0,0 +1,25 @@
+      $(document).ready(function(){
+       $(document).ready(function(){
+            $("#button1").click(function(){
+			$("#prod_123").animate_from_to("#cart");
+            });
+
+            
+      $("#button2").click(function(){
+	  $("#prod_123").animate_from_to("#cart2");
+         });
+            
+      $("#button3").click(function(){
+	  $("#prod_123").animate_from_to("#cart3");
+		});		
+			
+	   $("#button4").click(function(){
+	   $("#prod_123").animate_from_to("#cart4");
+		});
+
+	 });
+	  });
+      
+             
+            
+             
